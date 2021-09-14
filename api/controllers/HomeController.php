57c7bc8fad18c346939f6a02b145eb3e526e160a
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-14 20:15:00 +0800

    fix: Slide Show Image

--- api/controllers/HomeController.php
+++ api/controllers/HomeController.php
@@ -9,11 +9,19 @@ class HomeController extends BaseController
 {
   public function get()
   {
+    $slides = [
+      asset('img/slide1.jpg'),
+      asset('img/slide2.jpg'),
+      asset('img/slide3.jpg'),
+    ];
+
     $featured_products = Product::getFeaturedProducts();
 
     $view = new CustomerPageComponent("home_template");
     $view->setTitle("Home");
+    $view->addData("slides", $slides);
     $view->addData("featured_products", $featured_products);
+    $view->addScript(asset('js/slider.js'));
     $view->render();
   }
 }
--- api/templates/home_template.php
+++ api/templates/home_template.php
@@ -1,71 +1,45 @@
+<div class="slideshow relative w-full overflow-hidden" style="height: min(75vw, 75vh);">
 
-<div class="carousel relative container mx-auto" style="max-width:1600px;">
-  <div class="carousel-inner relative overflow-hidden w-full">
-    <!--Slide 1-->
-    <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-    <div class="carousel-item absolute opacity-0" style="height:50vh;">
-      <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('<?= BASE_URL ?>assets/img/slide1.webp');">
-
-        <div class="container mx-auto">
-          <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-            <p class="text-black text-2xl my-4">Stripy Zig Zag Jigsaw Pillow and Duvet Set</p>
-            <a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-    <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-    <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-    <!--Slide 2-->
-    <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-    <div class="carousel-item absolute opacity-0 bg-cover bg-right" style="height:50vh;">
-      <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('<?= BASE_URL ?>assets/img/slide2.webp');">
-
-        <div class="container mx-auto">
-          <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-            <p class="text-black text-2xl my-4">Real Bamboo Wall Clock</p>
-            <a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-    <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-    <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-    <!--Slide 3-->
-    <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-    <div class="carousel-item absolute opacity-0" style="height:50vh;">
-      <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-bottom" style="background-image: url('<?= BASE_URL ?>assets/img/slide3.webp');">
-
-        <div class="container mx-auto">
-          <div class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-            <p class="text-black text-2xl my-4">Brown and blue hardbound book</p>
-            <a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-    <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-    <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900  leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-    <!-- Add additional indicators for each slide-->
-    <ol class="carousel-indicators">
-      <li class="inline-block mr-3">
-        <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-      </li>
-      <li class="inline-block mr-3">
-        <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-      </li>
-      <li class="inline-block mr-3">
-        <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
-      </li>
-    </ol>
-
+  <?php foreach($slides as $slide) : ?>
+  <div class="slides absolute h-full w-full">
+    <img 
+      class="absolute top-0 left-0 right-0 bottom-0 mx-auto h-full w-full"
+      src="<?= $slide ?>"
+      >
   </div>
+  <?php endforeach; ?>
+
+  <!-- Left Button -->
+  <div class="absolute left-5 h-full">
+    <div class="flex flex-col justify-center h-full">
+      <a class="btn-left cursor-pointer text-gray-200 hover:text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+        </svg>
+      </a>
+    </div>
+  </div>
+
+  <!-- Right Button -->
+  <div class="absolute right-5 h-full">
+    <div class="flex flex-col justify-center h-full">
+      <a class="btn-right cursor-pointer text-gray-200 hover:text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+        </svg>
+      </a>
+    </div>
+  </div>  
+
+  <!-- Indicator -->
+  <div class="absolute w-full bottom-5">
+    <ul class="flex justify-center">
+      <?php foreach ($slides as $i => $slide) : ?>
+      <li class="indicator cursor-pointer mx-1 h-3 w-3 rounded-full border border-white"><a href="#"></a></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+
 </div>
 
 <div class="py-5">
