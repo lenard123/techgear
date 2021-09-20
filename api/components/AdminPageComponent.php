0eedd36fc64ae86f773a78644becbd8ff58e8736
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-20 21:10:35 +0800

    Initialize AdminHeader Component

--- api/components/AdminPageComponent.php
+++ api/components/AdminPageComponent.php
@@ -1,8 +1,15 @@
 <?php
 
 import("components/Component");
+import("components/AdminHeaderComponent");
 
 class AdminPageComponent extends Component
 {
   protected $template = "admin/admin_page_template";
+
+  public function __construct()
+  {
+    parent::addData("header", new AdminHeaderComponent);
+  }
+
 }
