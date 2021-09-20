e250ee2d60d1e08ac846238b94d9aee3bc73d9cc
lenard123 <lenard.mangayayam@gmail.com>
2021-09-20 08:21:00 +0000

    Use Production config

--- api/config.php
+++ api/config.php
@@ -1,15 +1,15 @@
 <?php
 
 //url || path
-define('BASE_URL', 'http://localhost/online-store/');
+define('BASE_URL', 'https://electronics-gadgets-ecommerce.herokuapp.com/');
 define('ROOT_PATH', dirname(__DIR__));
 define('API_PATH', __DIR__ . '/');
 
 //database
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_DATABASE', 'online-store');
+define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+define('DB_USER', 'ba88f006ccc135');
+define('DB_PASS', '4c6e529c');
+define('DB_DATABASE', 'heroku_670757a0b92e8ee');
 
 define('SITE_NAME', 'TechGear');
 define('SITE_SHIPPING_FEE', 38);
