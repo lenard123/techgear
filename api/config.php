bc6abea65a34d37f3c30c1a39435d8287366e857
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-22 16:38:04 +0800

    Fix development config

--- api/config.php
+++ api/config.php
@@ -1,15 +1,15 @@
 <?php
 
 //url || path
-define('BASE_FOLDER', '');
+define('BASE_FOLDER', 'online-store');
 define('ROOT_PATH', dirname(__DIR__));
 define('API_PATH', __DIR__ . '/');
 
 //database
-define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
-define('DB_USER', 'ba88f006ccc135');
-define('DB_PASS', '4c6e529c');
-define('DB_DATABASE', 'heroku_670757a0b92e8ee');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_DATABASE', 'online-store');
 
 define('SITE_NAME', 'TechGear');
 define('SITE_SHIPPING_FEE', 38);
