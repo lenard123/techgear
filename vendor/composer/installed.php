25e0fd7296ce7726b247ae262dcd8b9f69a2e19f
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-22 13:15:28 +0800

    Include Composer Autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f6ef24fff0e302cd06c852ca25ae7f841af0cdc0',
+    'name' => 'lenard/online-store',
+  ),
+  'versions' => 
+  array (
+    'lenard/online-store' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6ef24fff0e302cd06c852ca25ae7f841af0cdc0',
+    ),
+  ),
+);
