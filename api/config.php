f35944e5cb119654857bc61ef9a39422086e075c
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-28 11:23:08 +0800

    Add SITE_ENV in configuration

--- api/config.php
+++ api/config.php
@@ -11,6 +11,17 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_DATABASE', 'online-store');
 
+/**
+ * LOCAL | PRODUCTION
+ * 
+ * LOCAL
+ *  - Will use Js library in assets folder
+ * 
+ * PRODUCTION
+ *  - Will use Js library in cdn
+ * 
+ * */
+define('SITE_ENV', 'LOCAL');
 define('SITE_NAME', 'TechGear');
 define('SITE_SHIPPING_FEE', 38);
 
@@ -18,6 +29,4 @@ define('CACHE_ENABLED', false);
 define('CACHE_PROVIDER', 'file');
 define('CACHE_EXPIRATION', 60); //In minutes
 define('CACHE_FILE_DIR', ROOT_PATH . '/caches');
-
-define('ENV', 'DEBUG');
 
