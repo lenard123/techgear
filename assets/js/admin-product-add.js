4a78b2cdb99c207c331b1625cf5f93052d043a60
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-29 15:43:28 +0800

    Move public files on public folder

--- assets/js/admin-product-add.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-document.addEventListener('alpine:init', function () {
-  ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
-    console.log(editor);
-  })["catch"](function (error) {
-    console.error(error);
-  });
-});
