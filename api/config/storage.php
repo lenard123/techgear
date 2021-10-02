f46af64ee18bb7b10b66bb04f51988a626e4e944
Lenard  Mangay-ayam <31153286+lenard123@users.noreply.github.com>
2021-10-03 00:23:09 +0800

    Ftr cloudinary upload (#164)
    * Install Cloudinary Library
    
    * Downgrade Psr/log to fix cloudinary issue
    
    * Implement uploading of image to cloudinary
    
    * Fix Error when displaying image from cloudinary

--- api/config/storage.php
+++ api/config/storage.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-  'cloudinary_enabled' => false
+  'cloudinary_enabled' => env('STORAGE_CLOUDINARY_ENABLED', false),
+  'cloudinary_url' => env('STORAGE_CLOUDINARY_URL'),
+  'cloudinary_folder' => env('STORAGE_CLOUDINARY_FOLDER', 'techgear'),
 ];
