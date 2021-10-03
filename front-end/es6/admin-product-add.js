90e4e7713ada599504b64e9bf643affc8ea8b697
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-10-04 00:26:33 +0800

    Product Image Can now be preview before uploading

--- front-end/es6/admin-product-add.js
+++ front-end/es6/admin-product-add.js
@@ -1,5 +1,22 @@
 
 document.addEventListener('alpine:init', function () {
+
+  Alpine.data('productImagePreview', () => ({
+    source: null,
+
+    previewFile: function (e) {
+
+      const [file] = e.target.files
+
+      if (file) {
+        this.source = URL.createObjectURL(file);
+      }
+
+    }
+
+  }));
+
+
   ClassicEditor
   .create( document.querySelector( '#editor' ) )
   .then( editor => {
