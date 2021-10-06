f5b823e4ea0a4a510b90c1480312a0163dc70d63
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-10-06 11:22:26 +0800

    Create Icon Component

---
+++ api/Components/IconComponent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Components;
+
+class IconComponent extends BaseComponent
+{
+
+  protected $base_folder = 'templates/svgs/';
+
+  protected $defaultHeight = 32;
+  protected $defaultWidth = 32;
+
+  public function __construct($name, $classes = '')
+  {
+    $this->template = "$name.svg";
+    $this->addData('height', $this->defaultHeight);
+    $this->addData('width', $this->defaultWidth);
+    $this->addData('classes', $classes);
+  }
+
+  public function height($height)
+  {
+    $this->addData('height', $height);
+    return $this;
+  }
+
+  public function width($width)
+  {
+    $this->addData('width', $width);
+    return $this;
+  }
+
+  public function size($size)
+  {
+    return $this->height($size)->width($size);
+  }
+
+}
