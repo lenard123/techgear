776a6bd5368d05fde6fbaaf509108a2c5af7ab99
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-10-07 23:29:39 +0800

    Refactor Codes

--- api/templates/admin/signin_page.php
+++ api/templates/admin/signin_page.php
@@ -46,13 +46,10 @@
         </div>
 
 
-        <div class="flex items-center justify-between">
-
+        <div class="flex items-center">
           <!-- Remember me Input -->
-          <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-          </div>
+          <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+          <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
         </div>
 
         <div>
