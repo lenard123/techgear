fedf08868018bd4332ab7949a8a146d97e53b739
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-10-07 23:22:44 +0800

    Remove unnecessary codes

--- api/templates/admin/signin_page.php
+++ api/templates/admin/signin_page.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <meta name="description" content="Page not found">
   <title>Sign Up | TechGear Admin</title>
   <link rel="icon" type="image/jpeg" href="<?= url('assets/img/favicon.ico') ?>" />
   
@@ -11,9 +10,6 @@
   <link rel="stylesheet" type="text/css" href="<?= asset('css/all.css') ?>">
   <link rel="stylesheet" type="text/css" href="<?= asset('css/admin.css') ?>">
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Iceland&display=swap" rel="stylesheet"> 
 </head>
 
 <body class="bg-gray-100 px-4 sm:px-0">
