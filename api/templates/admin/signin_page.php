84197055957fded089a608d224914a6dd2583cb3
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-10-07 23:18:33 +0800

    Fix some design
    - Set the card width to 1/3 on large device
    - Change the Text Size to smaller

--- api/templates/admin/signin_page.php
+++ api/templates/admin/signin_page.php
@@ -19,14 +19,14 @@
 <body class="bg-gray-100 px-4 sm:px-0">
 
   <!-- Main Card -->
-  <main class="bg-white w-full sm:w-3/5 lg:w-2/5 mx-auto rounded-lg my-20 px-4 py-10 shadow-lg">
+  <main class="bg-white w-full sm:w-3/5 lg:w-1/3 mx-auto rounded-lg my-20 px-4 py-10 shadow-lg">
 
     <div class="max-w-md w-full mx-auto space-y-8">
 
       <!-- Brand Name and Logo -->
       <div>      
         <img class="mx-auto h-12 w-auto" src="<?= url('assets/img/favicon.ico') ?>" alt="Workflow">
-        <h2 class="mb-8 text-center text-3xl text-gray-700">
+        <h2 class="mb-8 text-center text-xl sm:text-2xl text-gray-700">
           Sign in to your account
         </h2>
       </div>
