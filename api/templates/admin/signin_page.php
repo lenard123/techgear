91cf7c585b5a211752d6b50a99afc781a9afb689
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-10-07 23:07:03 +0800

    Darken the Background Color

--- api/templates/admin/signin_page.php
+++ api/templates/admin/signin_page.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Iceland&display=swap" rel="stylesheet"> 
 </head>
 
-<body class="bg-white">
+<body class="bg-gray-100">
 
   <!-- Main Card -->
   <main class="bg-white w-full lg:w-1/3 mx-auto rounded-lg my-20 px-4 py-10 shadow-lg sm:w-2/3">
