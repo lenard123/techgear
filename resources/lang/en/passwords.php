d596bd6f043c24a6c0e1e6c554273740932cafdf
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2021-12-11 15:31:50 +0800

    Update password reset message

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'sent' => 'We have emailed your password reset link!. Check your spam folder if you havent received a mail from us.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
