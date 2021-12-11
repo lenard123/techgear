82628e489f363921ebc4ecc4fb4670efba7cabd9
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2021-12-12 00:07:26 +0800

    Bug Fixes

--- public/js/app.js
+++ public/js/app.js
@@ -5429,19 +5429,6 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/css/admin.css":
-/*!*********************************!*\
-  !*** ./resources/css/admin.css ***!
-  \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ }),
-
 /***/ "./node_modules/process/browser.js":
 /*!*****************************************!*\
   !*** ./node_modules/process/browser.js ***!
@@ -5758,8 +5745,7 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/app": 0,
-/******/ 			"css/admin": 0
+/******/ 			"css/app": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -5809,9 +5795,8 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app","css/admin"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/app","css/admin"], () => (__webpack_require__("./resources/css/app.css")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/admin"], () => (__webpack_require__("./resources/css/admin.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- resources/views/layouts/customer/index.blade.php
+++ resources/views/layouts/customer/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.main :title="$title" class="bg-gray-50">
+<x-layouts.main :title="$title" class="bg-gray-50 flex flex-col min-h-screen">
 
     @push('css')
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
@@ -7,7 +7,9 @@
     @include('layouts.customer.header')
     @include('layouts.customer.sidebar')
 
-    {{ $slot }}
+    <div class="flex-grow">
+        {{ $slot }}
+    </div>
 
     @include('layouts.customer.footer')
 
